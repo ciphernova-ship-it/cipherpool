f0119b85791cd5a3c46d121084b69e89956976be
ciphernova-ship-it <mohammad.numan@triremetrading.com>
2024-11-16 17:57:08 +0700

    added lit actions support

---
+++ enum/global.key.enum.js
@@ -0,0 +1,7 @@
+const globalKeyEnum = Object.freeze({
+    LIT_NODE_CLIENT: "LIT_NODE_CLIENT",
+    WALLET: "WALLET",
+    LIT_SESSION_SIGS: "LIT_SESSION_SIGS"
+})
+
+module.exports = globalKeyEnum
--- service/matching.engine.service.js
+++ service/matching.engine.service.js
@@ -1,19 +1,26 @@
 require("dotenv").config({path: "../.env"});
 const _ = require("lodash");
 
+const litLib = require("../lib/lit.lib");
 const mongoLib = require("../lib/mongo.lib");
+const walletLib = require("../lib/wallet.lib");
 const helperLib = require("../lib/helper.lib");
 const loggerLib = require("../lib/logger.lib");
 
+const litConfig = require("../config/lit.config.json");
 const orderModel = require("../model/order.model");
 
 (async () => {
     try {
+        await walletLib.initialiseWallet(process.env.MATCHING_ENGINE_PRIVATE_KEY);
+        await litLib.connect();
+        await litLib.generateSessionSigs();
+        const sessionSigs = await litLib.getSessionSigs();
         await mongoLib.connect(process.env.MONGO_URL);
 
-        let orders = [], pairOrders = [];
+        let orders = [], pairOrders = [], matchingCalls = [], matchingChecks = [];
         while (true) {
-            orders = await mongoLib.find(orderModel, {});
+            orders = await mongoLib.findWithSelect(orderModel, {}, {_id: 0, __v: 0});
 
             if (_.isEmpty(orders)) {
                 loggerLib.logInfo("No orders found!");
@@ -37,7 +44,24 @@ const orderModel = require("../model/order.model");
                 continue;
             }
 
-            //TODO: Implement matching logic here using lit protocol
+            matchingCalls = pairOrders.map((pairOrder) => litLib.executeLitAction(litConfig.orderMatchingLitActionCid, sessionSigs, {
+                order1AccessControlConditions: litLib.getOrderEncryptionAcc(pairOrder.fromOrder.maker),
+                order1Ciphertext: pairOrder.fromOrder.ciphertext,
+                order1DataToEncryptHash: pairOrder.fromOrder.dataToEncryptHash,
+                order2AccessControlConditions: litLib.getOrderEncryptionAcc(pairOrder.toOrder.maker),
+                order2Ciphertext: pairOrder.toOrder.ciphertext,
+                order2DataToEncryptHash: pairOrder.toOrder.dataToEncryptHash,
+            }));
+            matchingChecks = await Promise.all(matchingCalls);
+
+            pairOrders.forEach((pairOrder, index) => {
+                const matchingCheck = matchingChecks[index];
+                if (matchingCheck.response === "MATCHED") {
+                    loggerLib.logInfo("Match found!");
+                    loggerLib.logInfo(`From order: ${JSON.stringify(pairOrder.fromOrder)}`);
+                    loggerLib.logInfo(`To order: ${JSON.stringify(pairOrder.toOrder)}`);
+                }
+            });
         }
     } catch (error) {
         loggerLib.logError(error);
